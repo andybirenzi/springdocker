6aaab52531f03987c23c31c56be146386f3adc6a
Andy <andybirenzi@gmail.com>
2018-04-29 19:41:47 -0400

    add a post method

--- src/main/java/andy/birenzi/springdocker/Controller.java
+++ src/main/java/andy/birenzi/springdocker/Controller.java
@@ -1,6 +1,7 @@
 package andy.birenzi.springdocker;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,9 @@ public class Controller {
     @GetMapping("/")
     public String hello(){
         return "hello world from Docker";
+    }
+    @PostMapping("/")
+    public String postHello(){
+        return "Post hello world from Docker";
     }
 }
