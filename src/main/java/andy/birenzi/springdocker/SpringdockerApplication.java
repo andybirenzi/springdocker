5edf83114f0ca76b824d469544d87c276dd71732
Andy <andybirenzi@gmail.com>
2018-04-29 19:27:04 -0400

    initial commit

---
+++ src/main/java/andy/birenzi/springdocker/SpringdockerApplication.java
@@ -0,0 +1,12 @@
+package andy.birenzi.springdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdockerApplication.class, args);
+	}
+}
---
+++ src/test/java/andy/birenzi/springdocker/SpringdockerApplicationTests.java
@@ -0,0 +1,16 @@
+package andy.birenzi.springdocker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringdockerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
